c2fdfdd728783fcd68e94f24b52a4643f6b7c65d
stlemon <779174087@qq.com>
2018-01-16 16:39:10 +0800

    Update Constant.js

--- src/Constant.js
+++ src/Constant.js
@@ -5,6 +5,6 @@ export const MIN_COMPOSER_HEIGHT = Platform.select({
   android: 41,
 });
 export const MAX_COMPOSER_HEIGHT = 100;
-export const DEFAULT_PLACEHOLDER = 'Type a message...';
+export const DEFAULT_PLACEHOLDER = '请输入......';
 export const DATE_FORMAT = 'll';
 export const TIME_FORMAT = 'LT';
